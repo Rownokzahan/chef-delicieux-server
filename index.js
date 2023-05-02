58ac4e6885af09ed7371d66d96a47d435fe25ca2
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-02 18:30:32 +0600

    Add CORS middleware

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const chefs = require('./data/chefs.json')
+const cors = require('cors');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({ message: "Chef's Délicieux Server Is Running"})
