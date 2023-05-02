67d40cbd3c12d56b9eae6179b46cc6ece2db1a88
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-02 18:27:52 +0600

    Add chefs GET endpoints

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const chefs = require('./data/chefs.json')
+
 
 app.get('/', (req, res) => {
     res.json({ message: "Chef's Délicieux Server Is Running"})
+})
+
+app.get('/chefs', (req, res) => {
+    res.json(chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id
+    const chef = chefs.find(c => c.id === id)
+    res.json(chef)
 })
 
 app.listen(port, () => {
