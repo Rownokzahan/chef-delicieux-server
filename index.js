22cbfb2901bec668c70e3d423beb30e16bf22890
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-05-03 00:31:16 +0600

    Add endpoint to retrieve recipes by chef id

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const chefs = require('./data/chefs.json')
+const chefs = require('./data/chefs.json') 
+const recipes = require('./data/recipes.json') 
 const cors = require('cors');
 
 app.use(cors());
@@ -18,6 +19,12 @@ app.get('/chefs/:id', (req, res) => {
     const id = req.params.id
     const chef = chefs.find(c => c.id === id)
     res.json(chef)
+})
+
+app.get('/chefs/:chef_id/recipes', (req, res) => {
+    const chef_id = req.params.chef_id
+    const filteredRecipes = recipes.filter(recipe => recipe.chef_id === chef_id)
+    res.json(filteredRecipes)
 })
 
 app.listen(port, () => {
